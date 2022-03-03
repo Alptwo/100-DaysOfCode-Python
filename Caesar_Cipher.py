460033fa046aa61c70832c3f02470aa1d24a6b06
Alptwo <alptugaltin@gmail.com>
2022-03-03 16:49:13 +0300

    Did cryptation

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,16 +1,20 @@
-from cgitb import text
 
 def encrypt(text,shift):
     letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     
     crypted_text = ""
-
+    j=0
     for i in range(0,len(text)):
-        if(len(text)+shift <= 26):
-            text[i] = letters[i + shift]
-        else:
-            i%26
         
+        while(text[i]!=letters[j]):
+            j+=1
+
+        if(j+shift <= 26):
+            crypted_text = crypted_text + letters[(j%25)+shift]  
+        else:
+            crypted_text = crypted_text + letters[(j%25)+shift-1] 
+        j=0
+    print("Crypted text: " + crypted_text)
 
 
 
