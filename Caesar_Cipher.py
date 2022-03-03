4d8b1cc3cdc7205d6b9f18f6f9a3f5cd66205f2e
Alptwo <alptugaltin@gmail.com>
2022-03-03 18:05:54 +0300

    Uploaded ceaser cipher

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -2,7 +2,7 @@
 def encrypt(text,shift):
     letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     
-    crypted_text = ""
+    encrypted_text = ""
     j=0
     for i in range(0,len(text)):
         
@@ -10,18 +10,37 @@ def encrypt(text,shift):
             j+=1
 
         if(j+shift <= 26):
-            crypted_text = crypted_text + letters[(j%25)+shift]  
+            encrypted_text = encrypted_text + letters[(j%25)+shift]  
         else:
-            crypted_text = crypted_text + letters[(j%25)+shift-1] 
+            encrypted_text = encrypted_text + letters[(j%25)+shift-1] 
         j=0
-    print("Crypted text: " + crypted_text)
+    return encrypted_text
 
+def decrypt(encrypted_text,shift):
+    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
+    decrypted_text = ""
+    j=0
+    for i in range(0,len(encrypted_text)):
 
+        while(encrypted_text[i]!=letters[j]):
+            j+=1
+        
+        if(j-shift >= 0):
+            decrypted_text = decrypted_text + letters[(j%25)-shift]
+        else:
+            decrypted_text = decrypted_text + letters[(j+26)-shift]
+        j=0
 
-
+    return decrypted_text
+            
 
 
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-encrypt(text,shift)
+print(encrypt(text,shift))
+
+crypted_text = input("Type your crypted message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+print(decrypt(crypted_text,shift))
+
